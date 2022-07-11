3c53342f6be0ef64241bad30e6fe3e09fca8fcb6
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-07-11 23:33:31 +0000

    hotFix:

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,12 @@
+#!/usr/bin/python3
+"""A function that prints integers in reverse"""
+
+
 def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        my_list.reverse()
-        for el in my_list:
-            print('{:d}'.format(el))
+    if not my_list:
+        pass
+    else:
+        num = len(my_list) - 1
+        while num >= 0:
+            print("{:d}".format(my_list[num]))
+            num -= 1
