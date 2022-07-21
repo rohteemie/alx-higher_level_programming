301daa1185d1d4f7cd6da9c8db3b7cecb2c4a9be
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-07-21 21:33:18 +0000

    Solution-5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,41 +1,38 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 
 
 class Square:
-    """ creates a square and determines area and prints"""
-    def __init__(self, size=0):
-        """initializes instance of square
-        Args:
-        size: size of square"""
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
+    """Represent a square."""
 
-    def area(self):
-        """determines area"""
-        res = self.__size ** 2
-        return(res)
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
 
     @property
     def size(self):
-        """gets size"""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """sets size"""
         if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
     def my_print(self):
-        """prints square of size with # symbol"""
-        for i in range(self.__size):
-            print('#' * self.__size, end='')
-            print('')
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
         if self.__size == 0:
-            print('')
+            print("")
